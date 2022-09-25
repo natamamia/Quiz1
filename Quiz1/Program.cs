47782991abd64afc3bea1fcbc741cdcd3450b016
adrianaf <adrianaf@DESKTOP-BLMD6U1>
2022-09-25 18:59:40 +0400

    Add project files.

---
+++ Quiz1/Program.cs
@@ -0,0 +1,51 @@
+﻿// namespace Quiz1
+
+
+
+
+using System.Threading.Tasks;
+
+
+
+
+{ 
+
+
+    int num1, num2, num3, num4, num5;
+double z = 1;
+string phrase = "task";
+Console.WriteLine(phrase + z);
+    Console.WriteLine("The complete C# compilation process. It goes from C# code to Common Intermediate Language to native code.");
+
+
+double z = 2;
+string phrase = "task";
+Console.WriteLine(phrase +  z);
+
+double x = 5.3;
+double y = 3.6;
+Console.WriteLine(x + y);
+Console.WriteLine(x - y);
+Console.WriteLine(x * y);
+
+
+    double z = 3;
+    string phrase = "task";
+    Console.WriteLine(phrase + z);
+    
+
+
+    double z = 4;
+    string phrase = "task";
+
+    Console.WriteLine(phrase + z);
+
+
+    double z = 5;
+    string phrase = "task";
+
+    Console.WriteLine(phrase + z);
+
+    Console.ReadLine();
+}
+
